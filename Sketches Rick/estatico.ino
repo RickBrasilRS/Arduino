f45174c00de5bf4f2f97a460f86be6596378e4b7
Ricardo Maurina Roa <64716115+RickBrasilRS@users.noreply.github.com>
2020-05-26 01:07:12 -0300

    Update estatico.ino

--- Sketches Rick/estatico.ino
+++ Sketches Rick/estatico.ino
@@ -29,6 +29,7 @@ int contador1()
  preserva o valor de contador, o qual é passado de um escopo para outro
  Obs.: para atribuir a uma variável static um valor inicial diferente de zero, esse valor deve ser atribuído no momento da criação da veriável.
  Ex.: static int contador = 10;
+ Obs. 2: Nunca deixar da atribuir algum valor inicial para a variável. Mesmo o valor zero deve ser inicialmente atribuído, pois pode haver lixo na memória do microprocessador e essa prática poe evitar erros.
  ***/
 int contador2()
 {
