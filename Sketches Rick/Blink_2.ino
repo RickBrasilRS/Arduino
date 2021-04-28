4cfcde5dc514f55158021c0e38aa714a64bf6524
Ricardo Maurina Roa <rickfrater@gmail.com>
2021-04-28 00:45:02 -0300

    Blink_2 modificado para não usar mais a função dellay
    Blink_2 modificado para não usar mais a função dellay

--- Sketches Rick/Blink_2.ino
+++ Sketches Rick/Blink_2.ino
@@ -1,7 +1,10 @@
 // Blink 22
 // Colocar aqui variáveis iniciais do sistema e do sketch criado:
-  const int LED1 = 10, LED2 = 11; // Atribui aos LEDs 1 e 2 as portas 10 e 11 ao usar a função pinMode
+  const int LED1 = 10, LED2 = 11;  // Atribui aos LEDs 1 e 2 as portas 10 e 11 ao usar a função pinMode
+  const unsigned long intervalo = 400; // intervalo em milissegundos
   boolean flag1 = true, flag2 = false; // cria as variáveis booleanas fla1 e flag2
+  unsigned long momento1;
+  unsigned long momento2;
   
 void setup() 
 {
@@ -9,13 +12,19 @@ void setup()
   pinMode(LED2,OUTPUT);
   digitalWrite(LED1,HIGH); // acende o LED1 (poderia ser flag1 no lugar de HIGH)
   digitalWrite(LED2,LOW);  // apaga o LED2 (poderia ser flag2 no lugar de LOW)
+  momento1 = millis();
 }
 
 void loop() 
 {
-   delay(400);
-   flag1 = !flag1; // inverte o valor da variável (de true para false e vice-versa)
-   flag2 = !flag2;
-  digitalWrite(LED1,flag1);
-  digitalWrite(LED2,flag2);
+   momento2 = momento1 + intervalo; // substitui a função dellay sem parar o processamento da CPU
+   if (momento1 >= momento2)
+   {
+    flag1 = !flag1; // inverte o valor da variável (de true para false e vice-versa)
+    flag2 = !flag2;
+    digitalWrite(LED1,flag1);
+    digitalWrite(LED2,flag2);
+    momento1 = momento2;
+   }
+   
 }
